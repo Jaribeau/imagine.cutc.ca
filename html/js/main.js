b915d5b5108f95e7e93ba77ccc60985cbe1df69c
Jared Baribeau <jared.baribeau@gmail.com>
2016-02-03 02:25:14 -0500

    Added event icons, fixed scroll animate, edited text context all over.

--- html/js/main.js
+++ html/js/main.js
@@ -33,6 +33,20 @@ jQuery(function($) {'use strict',
 	}
 
 	menuToggle();
+
+	$( window ).resize(function() {
+		menuToggle();
+	});
+
+	$('.main-nav ul').onePageNav({
+		currentClass: 'active',
+	    changeHash: false,
+	    scrollSpeed: 900,
+	    scrollOffset: 0,
+	    scrollThreshold: 0.3,
+	    filter: ':not(.no-scroll)'
+	});
+
 	
 
 	// Signup Form.
