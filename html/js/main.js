a5f79214bdec67ea3960eb22596cbd2266e09c8d
Jared Baribeau <jared.baribeau@gmail.com>
2016-02-18 01:12:05 -0500

    About section split into tabs.

--- html/js/main.js
+++ html/js/main.js
@@ -10,6 +10,10 @@ jQuery(function($) {'use strict',
 			// callback function
 		});
 	
+	//Tab Switching in About section
+	jQuery(function () {
+	    jQuery('#aboutTabs a:last').tab('show')
+	})
 
 	
 	//Scroll Menu
